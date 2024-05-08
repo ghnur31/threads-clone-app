60f2abf5483122dbed056d4e9c77f17441da56ee
ghnur31 <ghidayatnur@gmail.com>
2024-05-08 11:25:58 +0700

    added suggested users feature

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { signupUser, loginUser, followUnFollowUser, updateUser, getUserProfile, logoutUser, } = require("../controllers/userController");
+const { signupUser, loginUser, followUnFollowUser, updateUser, getUserProfile, logoutUser, getSuggestedUsers } = require("../controllers/userController");
 const router = express.Router();
 const protectRoute = require("../middlewares/protectRoute");
 
 router.get('/profile/:query', getUserProfile);
+router.get("/suggested", protectRoute, getSuggestedUsers);
 router.post('/signup', signupUser);
 router.post('/login', loginUser);
 router.post('/logout', logoutUser);
